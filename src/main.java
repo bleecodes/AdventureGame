050e92728f74ddfd3476beadc5783e7108d14ff0
Brian Lee <writeto.blee@gmail.com>
2016-09-01 23:24:55 -0400

    init commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by BLEED on 9/1/2016.
+ */
+public class main {
+    public static void main (String [] args) {
+        System.out.println("cloned repo from github first");
+    }
+}
+
